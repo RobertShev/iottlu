86bf5092e576c71049213c39b6a22a91e8453da5
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-04 12:47:39 +0200

    actions added

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+    if(isset($_POST['sees'])){
+        $f=fopen("tulukekodune.txt", "w");
+        fwrite($f, 1);
+        fclose($f);
+    }
+    else if(isset($_POST['valjas'])){
+        $f=fopen("tulukekodune.txt", "w");
+        fwrite($f, 0);
+        fclose($f);
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,7 +17,7 @@
     </head>
     <body>
     <form method="POST" action="<?=$_SERVER['PHP_SELF'];?>">
-        <input type="button" name="sees">
-        <input type="button" name="valjas">
+        <input type="submit" name="sees" value="sees">
+        <input type="submit" name="valjas" value="väljas">
     </body>
 </html>
