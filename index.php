ce8abcc3b21a579b6d458bca3a9bab0c04f33de5
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-04 11:11:29 +0200

    kippar naide

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    if(isset($_REQUEST["valgus"])){
+        $f=fopen("valgusandmed1.txt" , "a");
+        fwrite($f, intval($_REQUEST["valgus"]).",".date("Y-m-d h:i:s")."\n");
+        fclose($f);
+        echo("Salvestati $_REQUEST[valgus]");
+    }else{
+        echo("Valguse andmed puuduvad");
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
