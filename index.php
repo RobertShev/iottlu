386a282e475f8d55aef1e6a50b6ffff740e966c2
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-04 12:41:37 +0200

    idex

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
-<?php
-    if(isset($_REQUEST["valgus"])){
-        $f=fopen("valgusandmed1.txt" , "a");
-        fwrite($f, intval($_REQUEST["valgus"]).",".date("Y-m-d h:i:s")."\n");
-        fclose($f);
-        echo("Salvestati $_REQUEST[valgus]");
-    }else{
-        echo("Valguse andmed puuduvad");
-    }
-?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Basic Web Page</title>
+        <title>Tuluke kontroll</title>
     </head>
     <body>
+    <form method="POST" action="<?=$_SERVER['PHP_SELF'];?>">
+        <input type="button" name="sees">
+        <input type="button" name="valjas">
     </body>
 </html>
