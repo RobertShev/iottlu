4b8ffebbb9ffc9d7aded805a46a1e0926b2a84f8
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-11 11:43:51 +0200

    Update dht22.php
    ilus

--- dht22.php
+++ dht22.php
@@ -1,10 +1,10 @@
 <?php
-    $hum = 'niiskus: ' . htmlspecialchars($_GET["niiskus"]) . ',';
-    $temp = 'temperatuur: ' . htmlspecialchars($_GET["temp"]) . ',';
-    $ht = 'heat index: ' . htmlspecialchars($_GET["ht"]) . '';
+    $hum = 'niiskus: ' . htmlspecialchars($_GET["niiskus"]) . ', ';
+    $temp = 'temperatuur: ' . htmlspecialchars($_GET["temp"]) . ', ';
+    $ht = 'heat index: ' . htmlspecialchars($_GET["ht"]) . ', ';
+    $date = date("Y-m-d h-i-s");
+    $data = $hum.$temp.$ht.$date.';/n'
     $f=fopen("dht22.txt", "a"); 
-       fwrite($f, $hum);
-        fwrite($f, $temp);
-        fwrite($f, $ht);
+        fwrite($f, $data);
         fclose($f);
 ?>
