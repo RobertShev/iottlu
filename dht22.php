733fb4bd48c2b8ab5e9abbcf67e06fc467700a88
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-11 11:30:12 +0200

    Update dht22.php
    fdnkjfdnbjkl;fdglk;bm

--- dht22.php
+++ dht22.php
@@ -1,5 +1,28 @@
 <?php
-    echo 'niiskus: ' . htmlspecialchars($_GET["niiskus"]) . ',';
-    echo 'temperatuur: ' . htmlspecialchars($_GET["temp"]) . ',';
-    echo 'heat index: ' . htmlspecialchars($_GET["ht"]) . '';
+    echo 
+    echo 
+    echo 
+?>
+<?php
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $f=fopen("dht22.txt", "a");
+       $hum = 'niiskus: ' . htmlspecialchars($_GET["niiskus"]) . ',';
+       $temp = 'temperatuur: ' . htmlspecialchars($_GET["temp"]) . ',';
+       $ht = 'heat index: ' . htmlspecialchars($_GET["ht"]) . ''; 
+       fwrite($f, $hum);
+        fwrite($f, $temp);
+        fwrite($f, $ht);
+        fclose($f);
+    }
+}
+    if(isset($_GET['niiskus'])){
+        $f=fopen("tulukekodune.txt", "w");
+        fwrite($f, 1);
+        fclose($f);
+    }
+    else if(isset($_POST['valjas'])){
+        $f=fopen("tulukekodune.txt", "w");
+        fwrite($f, 0);
+        fclose($f);
+    }
 ?>
