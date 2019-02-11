5ae5b15cff53eaf6949c7688b3228ca60ce47f42
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-11 12:04:10 +0200

    main.php
    chart

--- dht22.php
+++ dht22.php
@@ -8,36 +8,3 @@
        fwrite($f, $data);
        fclose($f);
 ?>
-<head>
-    <script src="https://code.highcharts.com/highcharts.src.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-</head>
-<body>
-`    <table class="highchart" 
-    data-graph-container=".. .. .highchart-container" 
-    data-graph-type="line">
-    <caption>Column example</caption>
-    <thead>
-        <tr>
-        <th>Month</th>
-        <th>Sales</th>
-        <th>Benefits</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <td>January</td>
-        <td>8000</td>
-        <td>2000</td>
-        </tr>
-    
-    ...
-    
-    </tbody>
-    </table>
-    <script>
-        $(document).ready(function() {
-        $('table.highchart').highchartTable();
-        });
-    </script>
-</body>
