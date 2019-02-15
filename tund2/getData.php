0fa85eb29aea7a487af8a2e2dee284cbcc37337e
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-15 10:46:15 +0200

    splitting data and dates

--- tund2/getData.php
+++ tund2/getData.php
@@ -1,11 +1,15 @@
 <?php
     $data = array();
+    $dates = array();
     $file = fopen("dht22raw.txt","r");
     while(! feof($file))
-    {
-        array_push($data,fgets($file));
+    {   
+        $value = explode(|,fgets($file))
+        echo($value[0]);
+        array_push($data,$value[0]);
+        array_push($data,$value[1]);
     }
     fclose($file);
-    print_r($data);
+    //print_r($data);
     //echo json_encode($data);
 ?> 
