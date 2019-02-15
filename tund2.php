05dc6e59c1b70a8c31e82a3c8f2a44294774dcc9
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-16 00:02:51 +0200

    date picker

--- tund2.php
+++ tund2.php
@@ -4,18 +4,35 @@
     </li>
     <li class="breadcrumb-item active">Tund 2</li>
 </ol>
-  <!-- Page level plugin JavaScript-->
-  <script src="vendor/chart.js/Chart.min.js"></script>
-  
-  <!-- Demo scripts for this page-->
-  <script src="js/demo/chart-area-demo.js"></script>
+
+<div class="breadcrumb">
+    <div class="form-group row">
+        <label for="start-date-input" class="col-2 col-form-label">Alates</label>
+        <div class="col-10">
+            <input class="form-control" type="date" value="2011-08-19" id="start-date-input">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="end-date-input" class="col-2 col-form-label">Kuni</label>
+        <div class="col-10">
+            <input class="form-control" type="date" value="2018-08-19" min="2018-01-01" max="2018-12-31" id="end-date-input">
+        </div>
+    </div>
+</div>
+<!--Chart Part-->
 <div class="card mb-3">
     <div class="card-header">
         <i class="fas fa-chart-area"></i>
-    Area Chart Example</div>
+    Niiskus</div>
     <div class="card-body">
         <canvas id="myAreaChart" width="100%" height="30"></canvas>
     </div>
     <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
 </div>
+
+<!-- Page level plugin JavaScript-->
+<script src="vendor/chart.js/Chart.min.js"></script>
+ 
+<!-- Demo scripts for this page-->
+<script src="js/demo/chart-area-demo.js"></script>
 <?php if($_GET['rel']!='tab'){ echo "</div>";} ?>
