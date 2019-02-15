588f19a1fa2f4563c44d6a7e0fa92cb2a0e15a5c
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-15 10:47:34 +0200

    revert changes

--- tund2/getData.php
+++ tund2/getData.php
@@ -1,15 +1,11 @@
 <?php
     $data = array();
-    $dates = array();
     $file = fopen("dht22raw.txt","r");
     while(! feof($file))
-    {   
-        $value = explode(|,fgets($file))
-        echo($value[0]);
-        array_push($data,$value[0]);
-        array_push($data,$value[1]);
+    {
+        array_push($data,fgets($file));
     }
     fclose($file);
-    //print_r($data);
-    //echo json_encode($data);
+    print_r($data);
+    echo json_encode($data);
 ?> 
