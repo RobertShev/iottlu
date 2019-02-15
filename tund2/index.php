b693789279cfd3044ada6896f090ad2a3664d21d
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-02-15 10:21:53 +0200

    Structured data

--- tund2/index.php
+++ tund2/index.php
@@ -1,9 +1,51 @@
 <?php
-    $file = fopen("dht22taw.txt","r");
-    while(! feof($file))
-    {
-        echo fgets($file). "<br />";
+    if(isset($_GET["kp"] && $_GET["kp2"])){
+        $f=fopen("dht22.txt", "a");
+        $kp = $_GET["kp"];
+        $kp2 = $_GET["kp2"];
+        $data = [];
+        foreach($f as $line){
+            $splitline = $line.explode("|",$line);
+            $date = strtotime($splitline[1]);
+            if($date>=$kp && $date<=$kp2){
+                array_push($data, $line)
+            }
+        }
     }
-    fclose($file);
-?> 
-hello world!
+
+    $f=fopen("dht22.txt", "a");
+    $data = [];
+    foreach($f as $line){
+        array_push($data, $line);
+    }
+?>
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
