200c7f39c622e47128eb72869cd1043e02b4702a
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-03-11 12:01:31 +0200

    ingame actions created

---
+++ pages/tund3/ingame.php
@@ -0,0 +1,72 @@
+<?php
+    $player1 = htmlspecialchars($_GET["player1"]);
+    $player2 = htmlspecialchars($_GET["player2"]);   
+    $players = fopen("players.txt","w");
+    $mode = fopen("mode.txt", "w");
+    $gameMode = "";
+
+    if($player1='1' && $player2='1'){
+        startGame("LvsL");
+    }else if($player1='1' || $player2='1'){
+        startGame("LvsO");
+    }else{
+        startGame("OvsO");
+    }
+    
+    function startGame($gameMode){
+        
+        fwrite($mode, $gameMode);
+        fclose($mode);
+        
+        $game=1;
+        $playerOneResponse=0;
+        $playerTwoResponse=1;
+
+        while($game==1){
+            if($playerTwoResponse==1){
+                while($playerOneResponse==0){
+                    $playerOneResponse=askResponse(1);
+                }
+            }
+            if($playerOneResponse==1){
+                while($playerTwoResponse==0){
+                    $playerTwoResponse=askResponse(2);
+                }
+            }
+            $game=decideWinner();
+
+        }
+    }
+
+    function askResponse($player){
+        switch ($player){
+            case 1:
+                $responseOne = htmlspecialchars($_GET["location"]);
+                if(checkLocation($responseOne)==1){
+                    if(saveResponse($responseOne)==1){
+                        return 1;
+                    }
+                    
+                }
+            case 2:
+                $responseTwo = htmlspecialchars($_GET["location"]);
+                if(checkLocation($responseTwo)==1){
+                    if(saveResponse($responseTwo)==1){
+                        return 1;
+                    }
+                }
+        }
+    }
+
+    function checkLocation($location){
+        return 1;
+    }
+
+    function saveResponse($location){
+        return 1;
+    }
+    //create better function name, instead of decideWinner
+    function decideWinner(){
+        return 1;
+    }
+?>
