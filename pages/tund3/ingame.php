4e2d964f301320e540dfe665c0dae0b4c4ca72d0
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2019-03-18 11:50:35 +0200

    winner deciding logic created

--- pages/tund3/ingame.php
+++ pages/tund3/ingame.php
@@ -1,9 +1,13 @@
 <?php
     $player1 = htmlspecialchars($_GET["player1"]);
-    $player2 = htmlspecialchars($_GET["player2"]);   
+    $player2 = htmlspecialchars($_GET["player2"]);  
+
+    $winner; 
+    $gameMode;
+    
     $players = fopen("players.txt","w");
     $mode = fopen("mode.txt", "w");
-    $gameMode = "";
+    
 
     if($player1='1' && $player2='1'){
         startGame("LvsL");
@@ -33,7 +37,10 @@
                     $playerTwoResponse=askResponse(2);
                 }
             }
-            $game=decideWinner();
+            if(decideWinner()!=0){
+                $game = 0;
+                $winner = decideWinner();
+            }
 
         }
     }
@@ -43,14 +50,14 @@
             case 1:
                 $responseOne = htmlspecialchars($_GET["location"]);
                 if(checkLocation($responseOne)==1){
-                    if(saveResponse($responseOne)==1){
+                    if(saveResponse($responseOne,1)==1){
                         return 1;
                     }
                 }
             case 2:
                 $responseTwo = htmlspecialchars($_GET["location"]);
                 if(checkLocation($responseTwo)==1){
-                    if(saveResponse($responseTwo)==1){
+                    if(saveResponse($responseTwo,2)==1){
                         return 1;
                     }
                 }
@@ -59,12 +66,10 @@
 
     function checkLocation($location){
     /*
-        TO-DO:
-        * controll logic one more time, develop for location.txt as 012112020;
         * where 0 untaken, 1 taken by first player , 2 taken by second player;
         * each number place represents location from 0 to 8 (9 locations);
     */ 
-        $file = file_get_contents("locationTest.txt");
+        $file = file_get_contents("location.txt");
         $locations = str_split($file);
         if($locations[$location]==0){
             return 1;
@@ -73,14 +78,106 @@
         }  
     }
 
-    function saveResponse($location){
-        // save to location.txt in form of 012112020;
+    function saveResponse($location,$player){
+        $file = file_get_contents("location.txt");
+        substr_replace($file, $player, $location, 0);
         return 1;
     }
 
-    //create better function name, instead of decideWinner
     function decideWinner(){
-        // compare all possible ways of winning with current state of locations
-        return 1;
+        $file = file_get_contents("location.txt");
+        $locations = str_split($file);
+        while($locationsAvailable){
+            for($i=0; $i<9; $i++){
+                if($locations[$i]=0){
+                    $locationsAvailable=true;
+                    break;
+                }else{
+                    $locationsAvailable=false;
+                    return -1;
+                }
+            }
+            if($locations[0] == $locations[1] && $locations[1] == $locations[2]){
+                switch($location[0]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+                
+            }else if($locations[3] == $locations[4] && $locations[4] == $locations[5]){
+                switch($location[3]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+    
+            }else if($locations[6] == $locations[7] && $locations[7] == $locations[8]){
+                switch($location[6]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+    
+            }else if($locations[0] == $locations[3] && $locations[3] == $locations[6]){
+                switch($location[0]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+    
+            }else if($locations[1] == $locations[4] && $locations[4] == $locations[7]){
+                switch($location[0]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+    
+            }else if($locations[2] == $locations[5] && $locations[5] == $locations[8]){
+                switch($location[0]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+    
+            }else if($locations[0] == $locations[4] && $locations[4] == $locations[8]){
+                switch($location[0]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+    
+            }else if($locations[2] == $locations[4] && $locations[4] == $locations[6]){
+                switch($location[0]){
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 0:
+                        return 0;
+                }
+            }else{return 0;}
+        }
+        
     }
 ?>
